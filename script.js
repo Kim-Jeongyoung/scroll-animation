26928c744c3b89e80a0f6a88605242b507e1102d
Kim-Jeongyoung <jeongyoung2@gmail.com>
2021-07-28 18:36:35 -0700

    add content

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const boxes = document.querySelectorAll('.box');
-
+const scrollContainer = document.querySelector('.scroll-animation-content');
 window.addEventListener('scroll', checkBoxes);
 
 checkBoxes();
+
+// function checkBoxes() {
+//   const triggerBottom =
+//     scrollContainer.getBoundingClientRect().top + window.scrollY;
+
+//   console.log('trigger bottom', triggerBottom);
+
+//   boxes.forEach((box) => {
+//     const boxTop = box.getBoundingClientRect().top;
+
+//     console.log('boxtop', boxTop);
+
+//     if (boxTop < triggerBottom) {
+//       box.classList.add('show');
+//     } else {
+//       box.classList.remove('show');
+//     }
+//   });
+// }
 
 function checkBoxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
